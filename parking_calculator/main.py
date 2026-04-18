91b945285555d91e87c71b703ba5f97071b1818c
Forgács Péter <forgacs.peter08@gmail.com>
2026-04-18 20:58:31 +0200

    logika és kiírás módosítása
    eddig ha a parkolás 1 napnál hosszabb volt, az öt órás díjat számolta fel, most már újrakezdi, és minden nap kap újra 30p ingyenes parkolást, és az első 3 óra is kedvezményes

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -7,19 +7,24 @@ def parkoloOra(idotartam) -> int:
     if(idotartam <= timedelta(minutes=30)):
         return 0
     
-    fizetendoIdoOraban = math.ceil((idotartam.total_seconds() - 1800) / 3600)
+    parkoltIdo = idotartam.total_seconds()
+
+    parkoltNapok = parkoltIdo // 86400
+    maradekIdo = parkoltIdo % 86400
+    fizetendoIdoOraban = math.ceil((maradekIdo - 1800) / 3600)
+    
+    if maradekIdo <= 1800:
+        return int(parkoltNapok * 10000)  
 
     if fizetendoIdoOraban <= 3:
-        return fizetendoIdoOraban * 300
+        return int(parkoltNapok * 10000 + fizetendoIdoOraban * 300)
 
-    if idotartam < timedelta(days=1):
-        return 3 * 300 + (fizetendoIdoOraban - 3) * 500
-    else: ## több mint 1 nap, ezért minden megkezdett óra + 500 forint mert régebb óta prakol mint 3 óra
-        return idotartam.days * 10000 + math.ceil(((idotartam.total_seconds() % 86400) - 1800) / 3600) * 500
-
+    return int(parkoltNapok * 10000 + 3 * 300 + (fizetendoIdoOraban - 3) * 500)
   
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
+
+    print(f"RENDSZAM\tDIJ")
 
     for sor in data.splitlines()[2:]:
         resz = sor.split()
@@ -27,7 +32,7 @@ def main():
         erkezes = datetime.strptime(resz[1] + " " + resz[2], "%Y-%m-%d %H:%M:%S")
         tavozas = datetime.strptime(resz[3] + " " + resz[4], "%Y-%m-%d %H:%M:%S")
 
-        print(f"{resz[0]} \t\t {parkoloOra(tavozas - erkezes)}")
+        print(f"{resz[0]}\t\t{parkoloOra(tavozas - erkezes)}")
 
 
 if __name__ == "__main__":
